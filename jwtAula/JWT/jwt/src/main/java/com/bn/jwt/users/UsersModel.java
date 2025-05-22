299eb1e3922781c9896aa2605fa32dfc26033ff3
victorcavalaro <100812378+victorcavalaro@users.noreply.github.com>
2025-05-22 19:39:46 -0300

    Add files via upload

---
+++ jwtAula/JWT/jwt/src/main/java/com/bn/jwt/users/UsersModel.java
@@ -0,0 +1,69 @@
+package com.bn.jwt.users;
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity(name = "users")
+public class UsersModel implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    private UserRole role;
+
+    public UsersModel(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return
+                List.of(new SimpleGrantedAuthority("ADMIN"),
+                new SimpleGrantedAuthority("USER"));
+           else  return  List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
